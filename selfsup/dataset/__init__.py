fa5d6742e672c763ea3c8199bb5ba5ee4889e42c
root <chagmgang@gmail.com>
2022-06-11 05:37:11 +0000

    add byol dataset

--- selfsup/dataset/__init__.py
+++ selfsup/dataset/__init__.py
@@ -1,4 +1,5 @@
 from .base import BaseDataset
 from .build import DATASETS, build_dataloader, build_dataset
+from .byol import BYOLDataset
 from .simclr import SimclrDataset
 from .test import TestDataset
