7367f934097ac2b875bdb28b5705b78db758bec9
root <chagmgang@gmail.com>
2022-06-24 07:36:15 +0000

    add swin

--- selfsup/models/backbone/__init__.py
+++ selfsup/models/backbone/__init__.py
@@ -2,3 +2,4 @@ from .convnext import BaseConvnext, LargeConvnext, TinyConvnext, XLargeConvnext
 from .resnet import (ResNet, ResNet18, ResNet34, ResNet50, ResNet101,
                      ResNet152, ResNext50_32x4d, ResNext101_32x8d,
                      WideResNet50_2, WideResNet101_2)
+from .swin import BaseSwin, LargeSwin, SmallSwin, TinySwin
