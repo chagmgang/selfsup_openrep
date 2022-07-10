9f6bdd17ece71c9000b162f155318c92de31b88a
root <chagmgang@gmail.com>
2022-07-10 15:38:06 +0000

    dino interpolate pos emb

--- selfsup/models/backbone/dino_vit.py
+++ selfsup/models/backbone/dino_vit.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.nn as nn
 
@@ -212,17 +214,19 @@ class VisionTransformer(nn.Module):
         h0 = h // self.patch_embed.patch_size
         w0, h0 = w0 + 0.1, h0 + 0.1
         patch_pos_embed = nn.functional.interpolate(
-            patch_pos_embed.reshape(
-                1, int(math.sqrt(N)), int(math.sqrt(N)), dim).permute(0, 3, 1, 2),
+            patch_pos_embed.reshape(1, int(math.sqrt(N)), int(math.sqrt(N)),
+                                    dim).permute(0, 3, 1, 2),
             scale_factor=(w0 / math.sqrt(N), h0 / math.sqrt(N)),
             mode='bicubic',
         )
-        assert int(w0) == patch_pos_embed.shape[-2] and int(h0) == patch_pos_embed.shape[-1]
+        assert int(w0) == patch_pos_embed.shape[-2] and int(
+            h0) == patch_pos_embed.shape[-1]
         patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
-        return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1)
-    
+        return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed),
+                         dim=1)
+
     def prepare_tokens(self, x):
-        B, nc ,w, h = x.shape
+        B, nc, w, h = x.shape
         x = self.patch_embed(x)
 
         cls_tokens = self.cls_token.expand(B, -1, -1)
